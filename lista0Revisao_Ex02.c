c0879d9c87a2db4f5576bc4481c128eae7bb5a33
LucasSaccoParisi <lucas.sparisi@senacsp.edu.br>
2025-08-14 15:20:53 -0300

    Add files via upload

---
+++ lista0Revisao_Ex02.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+  int tabuadas[] = {1, 2, 3, 4, 5, 6, 7, 8, 9}; //matriz para as tabuadas
+
+  for(int i = 0; i <= 9; i++){
+    printf("%d\n", tabuadas[i]);
+    // loop para fazer as contas da tabuada
+    // e imprimi-la
+  }
+  return 0;
+}
